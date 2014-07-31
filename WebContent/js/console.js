ed065c92edc934d3e53820074b940941c8530efa
luminocean <282896922@qq.com>
2014-07-31 15:40:17 +0800

    部分兼容性问题修正

--- WebContent/js/console.js
+++ WebContent/js/console.js
@@ -1,8 +1,7 @@
 /**
  * 控制台主逻辑
  */
- 
- 
+
 var currentLine;
 var engine = new Engine();
 
@@ -14,37 +13,44 @@ $(document).ready(function() {
 			var initFunc = function() {
 				currentLine = appendLineTo($("#console"));
 				setupKeyboardListener();
-				//setupTimer();
+				// setupTimer();
 			};
 
 			// 加载所有依赖的js脚本
-			//loadBatchJs(initFunc, "js/console.util.js");
-			
-			//由于已经在外部记载了，所以这里跳过加载直接启动
+			// loadBatchJs(initFunc, "js/console.util.js");
+
+			// 由于已经在外部记载了，所以这里跳过加载直接启动
 			initFunc();
 		});
-
 
 /**
  * 设置键盘监听
  */
 function setupKeyboardListener() {
+	//可显示字符部分
 	$(document).keypress(function(e) {
-				// $("#debug").append(e.keyCode);
-
 				// 可显示字符直接接在当前行后面
 				if (isDisplayable(e.which)) {
 					write(String.fromCharCode(e.which), currentLine);
 				}
+				
+			});
+
+	$(document).keydown(function(e) {
+				//$("#debug").append(e.keyCode);
+
 				// 回车键则换行
-				if (e.which == 13) {
+				if (e.keyCode == 13) {
 					typeInCommand(currentLine);
 					currentLine = appendLineTo($("#console"));
 				}
+				
 				// 删除键删除最后一个字符
-				if (e.which == 8) {
+				if (e.keyCode == 8) {
 					erase(String.fromCharCode(e.which), currentLine);
+					e.preventDefault();
 				}
+
 				// 左方向键
 				if (e.keyCode == 37) {
 					shiftCursor(false, currentLine);
@@ -56,11 +62,10 @@ function setupKeyboardListener() {
 			});
 }
 
+function typeInCommand(line) {
+	var command = extractCommand(line);
 
-function typeInCommand(line){
-	var command = extractCommand(line);	
-	
-	//将命令传入引擎开始执行
+	// 将命令传入引擎开始执行
 	engine.act(command);
 }
 
@@ -93,32 +98,31 @@ function loadBatchJs(callBack) {
  * 设置计时器，主要用于让光标周期性的闪烁
  */
 function setupTimer() {
-	//timer全局变量
+	// timer全局变量
 	timer = {
 		state : "cursorOn",
 		start : function() {
 			self.setTimeout("timer.switchState();", 800);
 		},
 		switchState : function() {
-			//只有在currentLine存在的时候才继续
+			// 只有在currentLine存在的时候才继续
 			if (currentLine) {
-				//目前是光标点亮状态
+				// 目前是光标点亮状态
 				if (this.state == "cursorOn") {
 					currentLine.children("p[id=cursor]")
 							.removeClass("cursorOn");
 					this.state = "cursorOff";
-					
+
 					self.setTimeout("timer.switchState()", 200);
-				}else if(this.state == "cursorOff" ){
-					currentLine.children("p[id=cursor]")
-							.addClass("cursorOn");
+				} else if (this.state == "cursorOff") {
+					currentLine.children("p[id=cursor]").addClass("cursorOn");
 					this.state = "cursorOn";
-					
+
 					self.setTimeout("timer.switchState()", 800);
 				}
 			}
 		}
 	};
-	
+
 	timer.start();
 }
